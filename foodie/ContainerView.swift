e86e3f59258a3594e2f0b5a01a58f156dc6ff09f
Greg <grjr02@outlook.com>
2019-05-06 01:34:47 -0400

    Container View

--- foodie/ContainerView.swift
+++ foodie/ContainerView.swift
@@ -8,23 +8,103 @@
 
 import UIKit
 
-class ContainerView: UIViewController {
+class ContainerView: UIViewController, VCDelegate {
+    
+//private var string1: String?
+//private var string2: String?
 
+
+    func passData(string1: String,string2:String ) {
+
+        //nameOfRestaurant?.text = string1
+        //addressOfRestaurant?.text = string2
+        
+        guard let nameOfRestaurant = nameOfRestaurant else { return }
+        guard let addressOfRestaurant = addressOfRestaurant else { return }
+        
+        UIView.transition(with: nameOfRestaurant, duration: 0.25, options: .transitionCurlUp, animations: {
+            nameOfRestaurant.text = string1
+
+        }, completion: nil)
+        UIView.transition(with: addressOfRestaurant, duration: 0.25, options: .transitionCurlUp, animations: {
+            addressOfRestaurant.text = string2
+        }, completion: nil)
+//        self.string1 = string1
+//        self.string2 = string2
+
+    }
+
+    
+    @IBOutlet weak var nameOfRestaurant: UILabel!
+    
+    @IBOutlet weak var urlOfRestaurant: UILabel!
+    
+    @IBOutlet weak var menuURLOfRestaurant: UILabel!
+    
+    @IBOutlet weak var addressOfRestaurant: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        //nameOfRestaurant.text = string1
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+ 
+//        rData.getLocation {
+//            zone,location  in
+//
+//            rData.getCuisine {
+//                cuisineName,cuisineId  in
+//
+//                rData.getCount(iD:cuisineId,cuisineZ:zone,locale:location){
+//                    start in
+//
+//                    rData.getRestaurants(iD:cuisineId,cuisineZ:zone,locale:location,startPoint:start){
+//                        locationVal,restaurantVal,menuVal,linkVal in
+//                        print("\(restaurantVal)\n\(locationVal)\n\(menuVal)\n\(linkVal)")
+//
+//                        DispatchQueue.main.async {
+//                            //Perform segue function with each variable
+//                            self.nameOfRestaurant.text = restaurantVal
+//                            self.menuURLOfRestaurant.text = menuVal
+//                            self.urlOfRestaurant.text = linkVal
+//                            self.addressOfRestaurant.text = locationVal
+//
+//                        }
+//
+//                    }
+//
+//                }
+//
+//            }
+//
+//
+//        }
+
+
+    
+    //}
+    
+
+  
+//}
--- foodie/ViewController.swift
+++ foodie/ViewController.swift
@@ -1,48 +1,110 @@
 import UIKit
-    
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var nameOfRestaurant: UILabel!
-    
-    @IBOutlet weak var urlOfRestaurant: UILabel!
-    
-    @IBOutlet weak var menuURLOfRestaurant: UILabel!
-    
-    @IBOutlet weak var addressOfRestaurant: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+protocol VCDelegate {
+    
+    //func passData(theData1:String,theData2:String,theData3:String,theData4:String)
+    func passData(string1:String,string2:String)
+
+}
+
+
+    class ViewController: UIViewController {
+
+    var delegate : VCDelegate?
+    
+    @IBAction func getRestaurantInformation(_ sender: Any) {
         
+
         rData.getLocation {
             zone,location  in
-            
+        
             rData.getCuisine {
                 cuisineName,cuisineId  in
-
+        
                 rData.getCount(iD:cuisineId,cuisineZ:zone,locale:location){
                     start in
-
+        
                     rData.getRestaurants(iD:cuisineId,cuisineZ:zone,locale:location,startPoint:start){
                         locationVal,restaurantVal,menuVal,linkVal in
-                        print("\(restaurantVal)\n\(locationVal)\n\(menuVal)\n\(linkVal)")
-                        
-                        DispatchQueue.main.async {
-                            
-                            self.nameOfRestaurant.text = restaurantVal
-                            self.menuURLOfRestaurant.text = menuVal
-                            self.urlOfRestaurant.text = linkVal
-                            self.addressOfRestaurant.text = locationVal
+                        //print("\(restaurantVal)\n\(locationVal)\n\(menuVal)\n\(linkVal)")
         
+                        DispatchQueue.main.async {
+        
+                            for child in self.children {
+                                if let child = child as? ContainerView {
+                                    child.passData(string1: restaurantVal,string2:locationVal)
+                                }
+                            }
+        
+                            
                         }
-                        
                     }
-
+        
                 }
+        
+            }
+        
+        }
 
+        
+        
+        
+        
+    }
+        
+        override func viewDidLoad() {
+            
+            
+            super.viewDidLoad()
+            
+        }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+            if let destination = segue.destination as? ContainerView {
+                destination.passData(string1: "",string2: "")
             }
             
         }
-
-    }
-
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if(segue.identifier == "segue"){
+//            self.delegate = segue.destination as! ContainerView
+//        }
+//    }
+
+//    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//        if(segue.identifier == "segue"){
+//            self.delegate = segue.destination as! ContainerView
+//        }
+//    }
+    
+    
+    
+        
+        
+    
+    
+
+        
+//    }
+
+
